d38d7467ba04ff0d58e89640b3cdadd879520331
Natan <natan.devback@gmail.com>
2024-06-06 14:17:12 -0300

    Testando o conceito de tupla

---
+++ Tuple.js
@@ -0,0 +1,7 @@
+"use strict";
+let coisas = ["Isaac", 10, true];
+let coisasAlteraveis = ["Isaac", 5, 10, true];
+coisasAlteraveis.push("Isaac", 5, 10, false);
+console.log(coisas[2]);
+console.log(coisas);
+console.log(coisasAlteraveis);
